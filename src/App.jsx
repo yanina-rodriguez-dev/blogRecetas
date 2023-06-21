433a2501eff37eaa6079f0152dc1c865af587879
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-06-20 23:48:02 -0300

    Maquetado footer terminado

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Login from "./views/Login";
 import Nav from "./common/Nav";
+import Footer from "./common/Footer";
 function App() {
     return (
         <>
             <Registro></Registro>
             <Nav></Nav>
             <Login></Login>
+            <Footer></Footer>
         </>
     );
 }
