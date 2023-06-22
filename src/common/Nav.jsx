6bdc39b48818c935836fb636ad3e51ec5df31e2a
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-22 12:26:11 -0300

    modifico rutas de logo principal, commiteo para descargar cambios

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link, NavLink} from 'react-router-dom';
 
 const Menu = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Cocinar es Amor</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>Cocinar es Amor</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
