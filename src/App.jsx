2182798a38892a62a06781e017f962448cf7a5a4
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-22 12:08:01 -0300

    termino de agregar y configurar lista de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Registro from "./views/Registro";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Login from "./views/Login";
-import Nav from "./common/Nav"
-import Administrador from './views/Administrador';
+import Nav from "./common/Nav";
+import Administrador from "./views/Administrador";
 import Inicio from "./views/Inicio";
 import Error404 from "./views/Error404";
 import Footer from "./common/Footer";
@@ -12,25 +12,41 @@ import EditarReceta from "./views/Receta/EditarReceta";
 import PaginaReceta from "./views/Receta/PaginaReceta";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
- 
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio></Inicio>}></Route>
-        <Route path="/administrador" element={<Administrador></Administrador>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/detalle-receta" element={<PaginaReceta></PaginaReceta>}></Route>
-        <Route path="/editar-receta" element={<EditarReceta></EditarReceta>}></Route>
-        <Route path="/crear-receta" element={<CrearReceta></CrearReceta>}></Route>
-        <Route path="/registro" element={<Registro></Registro>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Nav></Nav>
+        <Routes>
+          <Route exact path="/" element={<Inicio></Inicio>}></Route>
+          <Route
+            exact
+            path="/administrador"
+            element={<Administrador></Administrador>}
+          ></Route>
+          <Route exact path="/login" element={<Login></Login>}></Route>
+          <Route
+            exact
+            path="/detalle-receta"
+            element={<PaginaReceta></PaginaReceta>}
+          ></Route>
+          <Route
+            exact
+            path="/Administrador/editar-receta"
+            element={<EditarReceta></EditarReceta>}
+          ></Route>
+          <Route
+            exact
+            path="/Administrador/crear-receta"
+            element={<CrearReceta></CrearReceta>}
+          ></Route>
+          <Route exact path="/registro" element={<Registro></Registro>}></Route>
+          <Route path="*" element={<Error404></Error404>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
