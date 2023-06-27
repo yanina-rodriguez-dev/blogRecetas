691882cdf8833a24cd32276f73db9e864ba050e4
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-27 15:45:36 -0300

    agrego el codigo para mostrara mensajes cuando no se cumplka o cuando se cumpla la condicion

--- src/views/Receta/EditarReceta.jsx
+++ src/views/Receta/EditarReceta.jsx
@@ -1,15 +1,50 @@
+import { useEffect } from "react";
 import { Form, Button, FloatingLabel } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import { editarReceta, obtenerReceta } from "../../helpers/queries";
+import Swal from "sweetalert2";
 
 const EditarReceta = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
+    setValue,
   } = useForm();
+  const { id } = useParams();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  useEffect(() => {
+    obtenerReceta(id).then((respuesta) => {
+      if (respuesta) {
+        //carga en el formulario los datos del objeto
+        setValue("nombre", respuesta.nombreReceta);
+        setValue("imagen", respuesta.imagen);
+        setValue("descripcion", respuesta.descripcion);
+      }
+    });
+  }, []);
+
+  const onSubmit = (recetaEditada) => {
+    console.log(recetaEditada);
+    //AGREGAR LA CONSULTA A LA API QUE PIDE EDITAR
+    editarReceta(id, recetaEditada).then((respuesta) => {
+      if (respuesta.status === 200) {
+        Swal.fire(
+          "Producto creado",
+          `La receta ${recetaEditada.nombreReceta} fue EDITADA correctamente`,
+          "success"
+        );
+        reset();
+      } else {
+        Swal.fire(
+          "Ocurrio un error",
+          `La receta ${recetaEditada.nombreReceta} no pudo ser EDITADA`,
+          "error"
+        );
+      }
+    });
   };
 
   return (
@@ -48,8 +83,17 @@ const EditarReceta = () => {
             )}
           </Form.Group>
 
-          <FloatingLabel controlId="floatingTextarea2" label="Descripción de la receta">
-            <Form.Control as="textarea" style={{ height: "150px" }} />
+          <FloatingLabel
+            controlId="floatingTextarea2"
+            label="Descripción de la receta"
+          >
+            <Form.Control
+              as="textarea"
+              style={{ height: "150px" }}
+              {...register("descripcion", {
+                required: "La descripcion de la receta es obligatoria",
+              })}
+            />
           </FloatingLabel>
 
           <Button variant="primary" type="submit" className="mt-3">
--- src/views/Receta/ItemTablaReceta.jsx
+++ src/views/Receta/ItemTablaReceta.jsx
@@ -1,20 +1,30 @@
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const ItemTablaReceta = ({receta}) => {
-    return (
-        <tr>
-            <td>{receta.id}</td>
-            <td>{receta.nombreReceta}</td>
-            <td>{receta.imagen}</td>
-            <td>{receta.descripcion}</td>
-            <td>
-                <Button className="me-2 btn" variant="warning">
-                    Editar
-                </Button>
-                <Button variant="danger">Borrar</Button>
-            </td>
-        </tr>
-    );
+const ItemTablaReceta = ({ receta, handleBorrarReceta }) => {
+  const handleBorrarClick = () => {
+
+    
+    handleBorrarReceta(receta.id);
+  };
+
+  return (
+    <tr>
+      <td>{receta.id}</td>
+      <td>{receta.nombreReceta}</td>
+      <td>{receta.imagen}</td>
+      <td>{receta.descripcion}</td>
+      <td>
+        <Link className="me-2 btn btn-warning" variant="warning" to={`/administrador/editar-receta/${receta.id}`}>
+          Editar
+        </Link>
+        <Button variant="danger" onClick={handleBorrarClick}>
+          Borrar
+        </Button>
+      </td>
+    </tr>
+  );
 };
 
 export default ItemTablaReceta;
+
